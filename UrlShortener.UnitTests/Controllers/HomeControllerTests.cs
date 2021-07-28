1dfd37ebdff3b6ed617861eccd2ca14248a14e64
Terry Mace <terry.mace@ensek.co.uk>
2021-07-28 16:04:25 +0100

    Added test for index returning a view

---
+++ UrlShortener.UnitTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using UrlShortener.Controllers;
+using FluentAssertions;
+
+namespace UrlShortener.UnitTests.Controllers
+{
+    class HomeControllerTests
+    {
+        private HomeController _homeController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _homeController = new HomeController();
+        }
+
+        [Test]
+        public void Index_returns_ViewResult()
+        {
+            // Act
+            var result = _homeController.Index();
+
+            // Assert
+            result.Should().BeOfType<ViewResult>();
+        }
+    }
+}
