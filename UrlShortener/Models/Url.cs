773412055e56dac4bb1d39bd46394e4f5770037b
Terry Mace <terry.mace@ensek.co.uk>
2021-07-28 21:23:11 +0100

    Added client side validation rules

--- UrlShortener/Models/Url.cs
+++ UrlShortener/Models/Url.cs
@@ -1,7 +1,13 @@
-﻿namespace UrlShortener.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UrlShortener.Models
 {
     public class Url
     {
+        private const string urlRegex = @"[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)";
+
+        [Required(ErrorMessage = "Please enter a URL")]
+        [RegularExpression(urlRegex, ErrorMessage = "Please enter a valid URL.")]
         public string LongUrl { get; set; }
 
         public string ShortUrl { get; set; }
