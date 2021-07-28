c568b53b27bc088bb91034fed8413206d8838e39
Terry Mace <terry.mace@ensek.co.uk>
2021-07-28 15:35:12 +0100

    Clean up of initial setup

--- UrlShortener/UrlShortener/Controllers/HomeController.cs
+++ UrlShortener/UrlShortener/Controllers/HomeController.cs
@@ -1,12 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace UrlShortener.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
         public IActionResult Index()
         {
             return View();
