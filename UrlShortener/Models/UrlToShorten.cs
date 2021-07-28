11d57c49362cad225d22fcdb1f7d3ed72d531e26
Terry Mace <terry.mace@ensek.co.uk>
2021-07-28 21:44:50 +0100

    Refactored to be more descriptive and removed unneed property

--- UrlShortener/Models/UrlToShorten.cs
+++ UrlShortener/Models/UrlToShorten.cs
@@ -2,14 +2,12 @@
 
 namespace UrlShortener.Models
 {
-    public class Url
+    public class UrlToShorten
     {
         private const string urlRegex = @"[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)";
 
         [Required(ErrorMessage = "Please enter a URL")]
         [RegularExpression(urlRegex, ErrorMessage = "Please enter a valid URL.")]
-        public string LongUrl { get; set; }
-
-        public string ShortUrl { get; set; }
+        public string Url { get; set; }
     }
 }
