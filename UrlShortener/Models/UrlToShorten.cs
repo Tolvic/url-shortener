b7f4aa080cce8062d039c1d4b10ad0ce19c459b1
Terry Mace <terry.mace@ensek.co.uk>
2021-07-28 21:59:45 +0100

    Html Encoding URL for protection against XSS and injection

--- UrlShortener/Models/UrlToShorten.cs
+++ UrlShortener/Models/UrlToShorten.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Web;
 
 namespace UrlShortener.Models
 {
@@ -6,8 +7,13 @@ namespace UrlShortener.Models
     {
         private const string urlRegex = @"[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)";
 
+        private readonly string _url;
+
         [Required(ErrorMessage = "Please enter a URL")]
         [RegularExpression(urlRegex, ErrorMessage = "Please enter a valid URL.")]
-        public string Url { get; set; }
+        public string Url {
+            get => _url;
+            init => HttpUtility.HtmlEncode(value);
+        }
     }
 }
