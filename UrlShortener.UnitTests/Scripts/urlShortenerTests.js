22970686be2e217eb3c058366b93eb72c7dcf816
Terry Mace <terry.mace@ensek.co.uk>
2021-07-28 20:32:12 +0100

    Created urlShortener module to console log sucess or error

---
+++ UrlShortener.UnitTests/Scripts/urlShortenerTests.js
@@ -0,0 +1,64 @@
+﻿/// <reference path="../../UrlShortener/wwwroot/lib/jquery/jquery.js"/>
+/// <reference path="../../UrlShortener/wwwroot/lib/jquery-validation/jquery.validate.js"/>
+/// <reference path="../../UrlShortener/wwwroot/lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js"/>
+/// <reference path="../../UrlShortener/wwwroot/js/utilities.js"/>
+/// <reference path="../../UrlShortener/wwwroot/js/urlShortener.js"/>
+/// <reference path="../Lib/Jasmine/jasmine.js"/>
+/// <reference path="../Lib/Jasmine/jasmine-html.js"/>
+
+//These tests should be run using chutzpah
+
+
+describe("urlShortener.urlShortener", function () {
+
+    var utilitiesModule = urlShortener.utilities;
+    var urlShortenerModule = urlShortener.urlShortener;
+    var form, submitButton, urlField;
+
+    beforeEach(function () {
+        var mockHtml = getMockHtml();
+        var mockHtmlWithContainer = addMockHtmlContainer(mockHtml);
+        appendToDocumentBody(mockHtmlWithContainer);
+        form = $("#url-shortener");
+        submitButton = form.find("button");
+        urlField = $("#long-url");
+    });
+
+    afterEach(function () {
+        removeMockHtml();
+    });
+
+    it("should call urlShortener.utilities.ajaxFormInit when init is called", function () {
+        // arrange
+        spyOn(utilitiesModule, 'ajaxFormInit');
+
+        // act
+        urlShortenerModule.init();
+
+        // assert
+        expect(utilitiesModule.ajaxFormInit).toHaveBeenCalledWith(form, urlShortenerModule.sucessCallback, urlShortenerModule.errorCallback);
+    });
+
+    
+
+    function getMockHtml() {
+        return '<form id="url-shortener" action="exampleURl" method="POST">' +
+            '<input id="long-url" name="LongUrl" type="text" value="">' +
+            '<button type="submit" class="btn btn-std btn-primary mt-4">Get Short URL</button>' +
+            '</form>';
+    }
+
+    function addMockHtmlContainer(html) {
+        return '<div id="mock-html-container">' +
+            html +
+            '</div>';
+    }
+
+    function appendToDocumentBody(html) {
+        $(document.body).append(html);
+    }
+
+    function removeMockHtml() {
+        $("#mock-html-container").remove();
+    }
+});
---
+++ UrlShortener/wwwroot/js/urlShortener.js
@@ -0,0 +1,27 @@
+﻿var urlShortener = window.urlShortener || {};
+
+urlShortener.urlShortener = (function () {
+    function init() {
+        var form = getForm();
+        urlShortener.utilities.ajaxFormInit(form, sucessCallback, errorCallback);
+    }
+
+    function getForm() {
+        return $("#url-shortener");
+    }
+
+    function sucessCallback() {
+        console.log("Success");
+    }
+
+    function errorCallback() {
+        console.log("Error");
+
+    }
+
+    return {
+        init: init,
+        sucessCallback: sucessCallback,
+        errorCallback: errorCallback
+    }
+})();
