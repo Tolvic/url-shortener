0084adc70e4661eaa7e30c5f3e23a3b6b82908d5
Terry Mace <terry.mace@ensek.co.uk>
2021-07-29 21:28:48 +0100

    Added test to assert that model is encoding input

---
+++ UrlShortener.UnitTests/Models/UrlToShortenTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using UrlShortener.Models;
+
+namespace UrlShortener.UnitTests.Models
+{
+    class UrlToShortenTests
+    {
+        [Test]
+        public void ShouldEncodeUrlToBeShortened()
+        {
+            // Arrange
+            var maliciousInput = "https://test.com/<script>alert('Injected!');</script>";
+            var expectedUutputUrl = "https://test.com/&lt;script&gt;alert(&#39;Injected!&#39;);&lt;/script&gt;";
+
+            // Act
+            var urlToBeShortened = new UrlToShorten
+            {
+                Url = maliciousInput
+            };
+
+            // Assert
+            urlToBeShortened.Url.Should().Be(expectedUutputUrl);
+        }
+    }
+}
