229960a73bb362b2da249487218bcc13cea727b9
Terry Mace <terry.mace@ensek.co.uk>
2021-07-29 22:26:28 +0100

    Built a Random Url Generator

---
+++ UrlShortener.UnitTests/Services/RandomUrlGeneratorTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using UrlShortener.Services;
+
+namespace UrlShortener.UnitTests.Services
+{
+    class RandomUrlGeneratorTests
+    {
+        private RandomUrlGenerator _randomUrlGenerator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _randomUrlGenerator = new RandomUrlGenerator();
+        }
+
+        [Test]
+        public void Generate_ShouldReturnAStringOfTenCharacters()
+        {
+            // Act
+            var result = _randomUrlGenerator.Generate();
+
+            // Assert
+            result.Length.Should().Be(10);
+        }
+
+        [Test]
+        public void Generate_ShouldReturnUniqueStrings()
+        {
+            // Act
+            var result = _randomUrlGenerator.Generate();
+            var resultTwo = _randomUrlGenerator.Generate();
+
+            // Assert
+            result.Should().NotBe(resultTwo);
+        }
+
+        [Test]
+        public void Generate_WhenTwoSSeperateInstancesAreUsed_ShouldReturnUniqueStrings()
+        {
+            // Arrange
+            var randomUrlGeneratorTwo = new RandomUrlGenerator();
+
+            // Act
+            var result = _randomUrlGenerator.Generate();
+            var resultTwo = randomUrlGeneratorTwo.Generate();
+
+            // Assert
+            result.Should().NotBe(resultTwo);
+        }
+    }
+}
---
+++ UrlShortener/Services/RandomUrlGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UrlShortener.Services
+{
+    public class RandomUrlGenerator
+    {
+        private const int urlLength = 10;
+
+        private List<char> characters = new List<char>()
+        {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+        'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B',
+        'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+        'Q', 'R', 'S',  'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '-', '_'};
+
+        public string Generate()
+        {
+            string url = "";
+            Random rand = new Random();
+    
+            for (int i = 0; i < urlLength; i++)
+            {
+                var random = rand.Next(0, characters.Count);
+                url += characters[random].ToString();
+            }
+
+            return url;
+        }
+    }
+}
