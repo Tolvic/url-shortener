f6765a60998697807b155aae0b30c74daa17a9cd
Terry Mace <terry.mace@ensek.co.uk>
2021-07-29 23:40:40 +0100

    Created inital db migrtions

--- UrlShortener/Models/ShorenedtUrl.cs
+++ UrlShortener/Models/ShorenedtUrl.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace UrlShortener.Models
+﻿namespace UrlShortener.Models
 {
     public class ShortenedUrl : UrlToShorten
     {
+        public int Id { get; set; }
         public string ShortUrl { get; set; }
     }
 }
