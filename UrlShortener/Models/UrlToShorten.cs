2cec678abdf86eda00fce48d66cc4f8dc7b0367a
Terry Mace <terry.mace@ensek.co.uk>
2021-07-30 17:44:05 +0100

    Updated client side validation regex to be more inligned with backend validation

--- UrlShortener/Models/UrlToShorten.cs
+++ UrlShortener/Models/UrlToShorten.cs
@@ -5,12 +5,12 @@ namespace UrlShortener.Models
 {
     public class UrlToShorten
     {
-        private const string urlRegex = @"[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)";
+        private const string urlRegex = @"^http(s?):\/\/(www\.)?[a-zA-Z0-9@:%._\\+~#=]{2,256}.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)$";
 
         private readonly string _url;
 
         [Required(ErrorMessage = "Please enter a URL")]
-        [RegularExpression(urlRegex, ErrorMessage = "Please enter a valid URL.")]
+        [RegularExpression(urlRegex, ErrorMessage = "Please enter a valid URL. A Url must start with either https:// or  http:// and must end with a TLD such as .com or .co.uk")]
         public string Url {
             get => _url;
             init => _url = HttpUtility.HtmlEncode(value);
