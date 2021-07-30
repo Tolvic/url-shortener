4b7597ab2a005c100fe29b0e9c45ab93766959e8
Terry Mace <terry.mace@ensek.co.uk>
2021-07-30 17:23:45 +0100

    Implemented redirecting to short urls to long urls

--- UrlShortener.UnitTests/Controllers/HomeControllerTests.cs
+++ UrlShortener.UnitTests/Controllers/HomeControllerTests.cs
@@ -2,17 +2,24 @@
 using NUnit.Framework;
 using UrlShortener.Controllers;
 using FluentAssertions;
+using Moq;
+using UrlShortener.Repository;
+using UrlShortener.Models;
+using System.Linq;
 
 namespace UrlShortener.UnitTests.Controllers
 {
     class HomeControllerTests
     {
+        public const string _testDataShortUrl = "abcdefghi";
+        private Mock<IShortenUrlRepository> _MockShortenedUrlRepository;
         private HomeController _homeController;
 
         [SetUp]
         public void Setup()
         {
-            _homeController = new HomeController();
+            _MockShortenedUrlRepository = new Mock<IShortenUrlRepository>();
+            _homeController = new HomeController(_MockShortenedUrlRepository.Object);
         }
 
         [Test]
@@ -29,6 +36,79 @@ namespace UrlShortener.UnitTests.Controllers
 
             // Assert
             result.Should().BeOfType<ViewResult>();
+        }
+
+        [Test]
+        public void RedirectShortUrlToRealUrl_ShoudlBeDecoratedWithRouteAttribute()
+        {
+            // Act
+            var redirectShortUrlToRealUrlMethod = typeof(HomeController).Methods().Single(x => x.Name == "RedirectShortUrlToRealUrl");
+
+            // Assert
+            redirectShortUrlToRealUrlMethod.Should().BeDecoratedWith<RouteAttribute>(attr => attr.Template == "/{shortUrl}");
+        }
+
+        [Test]
+        public void RedirectShortUrlToRealUrl_MakesCallToShortenUrlReporsitory()
+        {
+            // Act
+            var result = _homeController.RedirectShortUrlToRealUrl(_testDataShortUrl);
+
+            // Assert
+            _MockShortenedUrlRepository.Verify(x => x.GetByShortUrl(_testDataShortUrl), Times.Once);
+        }
+
+        [Test]
+        public void RedirectShortUrlToRealUrl_RedirectsToAction_WhenShortenedUrlRepositoryReturnsNull()
+        {
+            // Arrange
+            _MockShortenedUrlRepository.Setup(x => x.GetByShortUrl(_testDataShortUrl)).Returns((ShortenedUrl)null);
+
+            // Act
+            var result = _homeController.RedirectShortUrlToRealUrl(_testDataShortUrl);
+
+            // Assert
+            result.Should().BeOfType<RedirectToActionResult>();
+        }
+
+        [Test]
+        public void RedirectShortUrlToRealUrl_RedirectsToIndex_WhenShortenedUrlRepositoryReturnsNull()
+        {
+            // Arrange
+            _MockShortenedUrlRepository.Setup(x => x.GetByShortUrl(_testDataShortUrl)).Returns((ShortenedUrl)null);
+
+            // Act
+            var result = _homeController.RedirectShortUrlToRealUrl(_testDataShortUrl) as RedirectToActionResult;
+
+            // Assert
+            result.ActionName.Should().Be("Index");
+        }
+
+        [Test]
+        public void RedirectShortUrlToRealUrl_Redirects_WhenShortenedUrlRepositoryReturnsValue()
+        {
+            // Arrange
+            _MockShortenedUrlRepository.Setup(x => x.GetByShortUrl(_testDataShortUrl)).Returns(new ShortenedUrl { Url = "https://google.com"});
+
+            // Act
+            var result = _homeController.RedirectShortUrlToRealUrl(_testDataShortUrl);
+
+            // Assert
+            result.Should().BeOfType<RedirectResult>();
+        }
+
+        [Test]
+        public void RedirectShortUrlToRealUrl_RedirectsToValue_WhenShortenedUrlRepositoryReturnsAValue()
+        {
+            // Arrange
+            var longUrl = "https://google.com";
+            _MockShortenedUrlRepository.Setup(x => x.GetByShortUrl(_testDataShortUrl)).Returns(new ShortenedUrl { Url = longUrl });
+
+            // Act
+            var result = _homeController.RedirectShortUrlToRealUrl(_testDataShortUrl) as RedirectResult;
+
+            // Assert
+            result.Url.Should().Be(longUrl);
         }
     }
 }
--- UrlShortener/Controllers/HomeController.cs
+++ UrlShortener/Controllers/HomeController.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using UrlShortener.Repository;
 
 namespace UrlShortener.Controllers
 {
     public class HomeController : Controller
     {
+        private IShortenUrlRepository _shortenedUrlRepositoy;
+
+        public HomeController(IShortenUrlRepository shortenedUrlRepository)
+        {
+            _shortenedUrlRepositoy = shortenedUrlRepository;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [Route("/{shortUrl}")]
+        public IActionResult RedirectShortUrlToRealUrl(string shortUrl)
+        {
+            var shortenedUrl = _shortenedUrlRepositoy.GetByShortUrl(shortUrl);
+
+            if (shortenedUrl == null)
+            {
+                return RedirectToActionPermanent("Index");
+            }
+
+            return RedirectPermanent(shortenedUrl.Url);
         }
     }
 }
